701a8d1c18205c690648677c2747fca855c9bebe
Gustavo Avide <g_a93@outlook.com>
2025-04-30 17:17:44 +0100

    feat: add pipeline and remove general no need stuff

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CodePipeline(this, 'AwesomePipeline', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('alexhddev/CDK-course-resources', 'cicd-practice'),
+        commands: [
+          'cd cdk-cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out'
+      })
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
